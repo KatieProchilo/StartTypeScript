3258de998298313a87018f8549e360488c786c9a
Katie Prochilo <43346846+KatieProchilo@users.noreply.github.com>
2019-04-25 07:57:48 +0900

    Created person and friend types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-const foo = 5;
+import { Friend, Person} from './types';
+
+const foo: number = 5;
 
 console.log(foo);
+
+const bill: Person = {
+  id: 5,
+  name: 'Bill'
+}
+
+const melinda: Friend = {
+  name: 'Melinda',
+  yearsOfFriendship: 9
+}
+
+bill.friends = [melinda];
+
+console.log(bill);
+
